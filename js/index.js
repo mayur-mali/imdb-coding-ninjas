e85e7943be9814952b0dc6617687a8f9a7767f2c
mayur mali <mayurmali31@gmail.com>
2022-09-22 17:47:57 +0530

    wip

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-console.log("hello world!");
+const searchbox = document.getElementById("searchBox");
+
+function getMovieData() {
+  fetch(`http://www.omdbapi.com/?s=${searchbox.value}&apikey=79cadf06`)
+    .then((response) => response.json())
+    .then((data) => {
+      if ((data.Response = "True")) {
+        console.log(data.Search);
+      }
+    });
+  //   console.log(searchbox.value);
+}
+
+function fetchMovie(data, delay) {
+  let timer;
+  return function (...args) {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      data();
+    }, delay);
+  };
+}
+
+const debounceFunction = fetchMovie(getMovieData, 1000);
