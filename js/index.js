5c89fa86511f3313477d4c670709878320f09d91
mayur-mali <mayurmali31@gmail.com>
2022-09-29 22:19:23 +0530

    final version

--- js/index.js
+++ js/index.js
@@ -26,9 +26,11 @@ function showMovies(movieData) {
 
   let div = "";
   if (movieData) {
+    //  {if you want to use on localhost just remove /imdb-coding-ninjas/ from a tag}
     movieData.forEach((movie) => {
       div += `
       <div class="bg-[#191a23] overflow-hidden rounded-xl relative p-4">
+
                         <a href="/imdb-coding-ninjas/movie-details.html?id=${movie.imdbID}">
                             <div class="h-48 relative">
                                 <img src="${movie.Poster}"
